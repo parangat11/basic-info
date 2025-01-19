4b91d3cae6084381d47ab799efe47e9499fb8afd
parangat11 <parangataditya001@gmail.com>
2025-01-19 15:30:34 +0530

    Add all the routes and their respective htmls

--- index.js
+++ index.js
@@ -1,6 +1,88 @@
+const path = require('path')
 let express = require('express')
 let app = express()
 
-// app.get("/", (req, res) => {
-//     res.sendFile()
-// })
+app.get("/", (req, res, next) => {
+    let options = {
+        root: path.join(__dirname),
+        dotfiles: 'deny',
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+    }
+    let fileName = "index.html"
+    res.sendFile(fileName, options, err => {
+        if(err) {
+            next(err)
+        }
+        else {
+            console.log('Sent: ', fileName)
+        }
+    })
+})
+
+app.get("/about", (req, res, next) => {
+    let options = {
+        root: path.join(__dirname),
+        dotfiles: 'deny',
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+    }
+    let fileName = "about.html"
+    res.sendFile(fileName, options, err => {
+        if(err) {
+            next(err)
+        }
+        else {
+            console.log('Sent: ', fileName)
+        }
+    })
+})
+
+app.get("/contact-me", (req, res, next) => {
+    let options = {
+        root: path.join(__dirname),
+        dotfiles: 'deny',
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+    }
+    let fileName = "contact-me.html"
+    res.sendFile(fileName, options, err => {
+        if(err) {
+            next(err)
+        }
+        else {
+            console.log('Sent: ', fileName)
+        }
+    })
+})
+
+app.get("/:name", (req, res, next) => {
+    let options = {
+        root: path.join(__dirname),
+        dotfiles: 'deny',
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+    }
+    let fileName = "404.html"
+    res.sendFile(fileName, options, err => {
+        if(err) {
+            next(err)
+        }
+        else {
+            console.log('Sent: ', fileName)
+        }
+    })
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
