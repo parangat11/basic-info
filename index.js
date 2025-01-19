2876e61a0e0bce6bf1a11a93aac3514d63649ee2
parangat11 <parangataditya001@gmail.com>
2025-01-19 15:02:53 +0530

    Add express in the dependencies, create server in index.js

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-let http = require('http')
-let fs = require('fs')
+let express = require('express')
+let app = express()
 
-http.createServer((req, res) => {
-    const str = req.url.slice(1)
-    switch(str) {
-        case '':
-            fs.readFile('index.html', (err, data) => {
-                res.writeHead(200, {'Content-type': 'text/html'})
-                res.write(data)
-                return res.end()
-            })
-            break;
-        case 'about':
-            fs.readFile('about.html', (err, data) => {
-                res.writeHead(200, {'Content-type': 'text/html'})
-                res.write(data)
-                return res.end()
-            })
-            break;
-        case 'contact-me':
-            fs.readFile('contact-me.html', (err, data) => {
-                res.writeHead(200, {'Content-type': 'text/html'})
-                res.write(data)
-                return res.end()
-            })
-            break;
-        default:
-            fs.readFile('404.html', (err, data) => {
-                res.writeHead(200, {'Content-type': 'text/html'})
-                res.write(data)
-                return res.end()
-            })
-    }
-}).listen(8080)
+// app.get("/", (req, res) => {
+//     res.sendFile()
+// })
